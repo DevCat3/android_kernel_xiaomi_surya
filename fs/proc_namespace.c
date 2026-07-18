b76d21aa86feee4dbbd23dd1763b1d8378e948b2
DevCat3 <omarelbakery2008@gmail.com>
2026-07-18 19:30:19 +0300

    Revert "fs: implement susfs v2.0.0"
    This reverts commit 7270786ff109269cfa4680624440bc142074058a.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -12,19 +12,11 @@
 #include <linux/security.h>
 #include <linux/fs_struct.h>
 #include <linux/sched/task.h>
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-#include <linux/susfs_def.h>
-#endif
 
 #include "proc/internal.h" /* only for get_proc_task() in ->open() */
 
 #include "pnode.h"
 #include "internal.h"
-
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_hide_sus_mnts_for_non_su_procs;
-extern bool susfs_is_current_ksu_domain(void);
-#endif
 
 static unsigned mounts_poll(struct file *file, poll_table *wait)
 {
@@ -110,15 +102,6 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	struct super_block *sb = mnt_path.dentry->d_sb;
 	int err;
 
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (susfs_hide_sus_mnts_for_non_su_procs &&
-			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
-			!susfs_is_current_ksu_domain())
-	{
-		return 0;
-	}
-#endif
-
 	if (sb->s_op->show_devname) {
 		err = sb->s_op->show_devname(m, mnt_path.dentry);
 		if (err)
@@ -155,6 +138,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	struct path mnt_path = { .dentry = mnt->mnt_root, .mnt = mnt };
 	int err;
 
+<<<<<<< HEAD
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	if (susfs_hide_sus_mnts_for_non_su_procs &&
 			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
@@ -164,6 +148,8 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	}
 #endif
 
+=======
+>>>>>>> parent of 7270786ff109 (fs: implement susfs v2.0.0)
 	seq_printf(m, "%i %i %u:%u ", r->mnt_id, r->mnt_parent->mnt_id,
 		   MAJOR(sb->s_dev), MINOR(sb->s_dev));
 	if (sb->s_op->show_path) {
@@ -228,6 +214,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	struct super_block *sb = mnt_path.dentry->d_sb;
 	int err;
 
+<<<<<<< HEAD
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	if (susfs_hide_sus_mnts_for_non_su_procs &&
 			r->mnt_id >= DEFAULT_KSU_MNT_ID &&
@@ -237,6 +224,8 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	}
 #endif
 
+=======
+>>>>>>> parent of 7270786ff109 (fs: implement susfs v2.0.0)
 	/* device */
 	if (sb->s_op->show_devname) {
 		seq_puts(m, "device ");
